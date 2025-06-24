b4458622ddbb070eed111838dc4d6c6a80616399
yoshida106 <stassiluk308@gmail.com>
2025-06-24 23:29:02 +0300

    Add files via upload

---
+++ 2. Сортування бульбашкою (Bubble Sort).cs
@@ -0,0 +1,23 @@
+    public static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            bool swapped = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            // If no elements were swapped, array is sorted
+            if (!swapped) break;
+        }
+    }
+}
