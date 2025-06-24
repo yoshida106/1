fc5ef285488f8810d442d33e69b19f2f0c3725ce
yoshida106 <stassiluk308@gmail.com>
2025-06-25 00:27:11 +0300

    Add files via upload

--- Тестування методів.cs
+++ Тестування методів.cs
@@ -2,54 +2,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Підготовка даних
-        double[] xValues = { -4, -2, 0, 2, 4 };
-        double[] yValues = { -37, -1, 11, -1, -37 };
-
-        // 1. Обчислення скінчених різниць
-        Console.WriteLine("1. Скінчені різниці:");
-        double[] finiteDifferences = NewtonInterpolation.CalculateFiniteDifferences(xValues, yValues);
-        for (int i = 0; i < finiteDifferences.Length; i++)
-        {
-            Console.WriteLine($"Δ^{i}f(x0) = {finiteDifferences[i]}");
-        }
-
-        // 2. Обчислення розділених різниць
-        Console.WriteLine("\n2. Розділені різниці:");
-        double[] dividedDifferences = NewtonInterpolation.CalculateDividedDifferences(xValues, yValues);
-        for (int i = 0; i < dividedDifferences.Length; i++)
-        {
-            Console.WriteLine($"f[x0..x{i}] = {dividedDifferences[i]}");
-        }
-
-        // 3. Інтерполяція для рівномірних вузлів
-        Console.WriteLine("\n3. Інтерполяція (рівномірні вузли):");
-        TestInterpolation((x) => NewtonInterpolation.NewtonUniform(
-            x, xValues, finiteDifferences));
-
-        // 4. Інтерполяція для нерівномірних вузлів
-        Console.WriteLine("\n4. Інтерполяція (нерівномірні вузли):");
-        TestInterpolation((x) => NewtonInterpolation.NewtonNonUniform(
-            x, xValues, dividedDifferences));
-    }
-
-    static void TestInterpolation(Func<double, double> interpolator)
-    {
-        // Тестові точки для перевірки
-        double[] testPoints = { -3, -1, 1, 3 };
-
-        foreach (double x in testPoints)
-        {
-            double interpolatedValue = interpolator(x);
-            Console.WriteLine($"f({x}) ≈ {interpolatedValue:F4}");
-        }
-
-        // Перевірка в вузлах інтерполяції
-        Console.WriteLine("\nПеревірка в вузлах:");
-        foreach (var point in NewtonInterpolation.TableData)
-        {
-            double interpolatedValue = interpolator(point.Key);
-            Console.WriteLine($"f({point.Key}) = {point.Value}, P({point.Key}) = {interpolatedValue}");
-        }
+        // Приклад системи рівнянь:
+        // 4x1 +  x2 -  x3 = 5
+        //  x1 + 3x2 +  x3 = 6
+        //  x1 - 2x2 + 5x3 = 7
+        
+        double[,] matrix = {
+            { 4,  1, -1 },
+            { 1,  3,  1 },
+            { 1, -2,  5 }
+        };
+        
+        double[] vector = { 5, 6, 7 };
+        
+        Console.WriteLine("Метод Гауса:");
+        double[] gaussSolution = LinearSystems.GaussMethod((double[,])matrix.Clone(), (double[])vector.Clone());
+        Console.WriteLine("\nОстаточний розв'язок:");
+        Console.WriteLine($"x1 = {gaussSolution[0]:F6}, x2 = {gaussSolution[1]:F6}, x3 = {gaussSolution[2]:F6}");
+        
+        Console.WriteLine("\n----------------------------------------\n");
+        
+        Console.WriteLine("Метод простих ітерацій (ε = 1e-6):");
+        double[] iterationSolution = LinearSystems.SimpleIterationMethod(matrix, vector, 1e-6);
+        Console.WriteLine("\nОстаточний розв'язок:");
+        Console.WriteLine($"x1 = {iterationSolution[0]:F6}, x2 = {iterationSolution[1]:F6}, x3 = {iterationSolution[2]:F6}");
     }
 }
